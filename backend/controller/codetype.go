67e23f2943d1833f47184f06c1b35b13703d87b5
Taweechai Poedee <taweechaipoedee8@gmail.com>
2023-01-07 15:12:32 +0700

    create controller codetype -close #90

---
+++ backend/controller/codetype.go
@@ -0,0 +1,20 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sut65/team10/entity"
+)
+
+// POST /users
+func ListCodetypes(c *gin.Context) {
+	var Codetype entity.Codetype
+	if err := entity.DB().Raw("SELECT * FROM codetypes").Scan(&Codetype).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": Codetype})
+
+}
