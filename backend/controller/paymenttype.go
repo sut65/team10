2782e2cd37980d5e8ea0853759e7ae67864463eb
Taweechai Poedee <taweechaipoedee8@gmail.com>
2023-01-07 15:02:40 +0700

    Create controller paymenttype -close #32

---
+++ backend/controller/paymenttype.go
@@ -0,0 +1,20 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sut65/team10/entity"
+)
+
+// POST /users
+func ListPaymenttypes(c *gin.Context) {
+	var Paymenttype entity.Paymenttype
+	if err := entity.DB().Raw("SELECT * FROM users").Scan(&Paymenttype).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": Paymenttype})
+
+}
