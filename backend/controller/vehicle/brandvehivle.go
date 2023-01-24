d2f4c05d3de16a3b42116f51e4831661365e3f4b
wijtra <wsgniy@gmail.com>
2023-01-24 22:19:27 +0700

    controller vehicle -close #126

---
+++ backend/controller/vehicle/brandvehivle.go
@@ -0,0 +1,20 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sut65/team10/entity"
+)
+
+// POST /brandvehicles
+func ListBrand_Vehicles(c *gin.Context) {
+	var brandvehicles entity.Brand_Vehicle
+	if err := entity.DB().Raw("SELECT * FROM reasons").Scan(&brandvehicles).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": brandvehicles})
+
+}
