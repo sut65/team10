eaf6aebeabc8d48d4049ff59366f938c19eb06b6
Taweechaikxmm <taweechaipoedee8@gmail.com>
2023-01-26 22:55:45 +0700

    update function generate Quotacode and controller quotacode -close #181

--- backend/controller/promotion/quotacode.go
+++ backend/controller/promotion/quotacode.go
@@ -7,10 +7,31 @@ import (
 	"github.com/sut65/team10/entity"
 )
 
-// POST /users
+// POST /Quotacodes
 func ListQuotacode(c *gin.Context) {
 	var quotacode []entity.QuotaCode
-	if err := entity.DB().Raw("SELECT * FROM quotacodes").Scan(&quotacode).Error; err != nil {
+	if err := entity.DB().Raw("SELECT * FROM quota_codes").Scan(&quotacode).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": quotacode})
+
+}
+
+func UpdateQuotacode(c *gin.Context) {
+	var quotacode entity.QuotaCode
+	if err := c.ShouldBindJSON(&quotacode); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if tx := entity.DB().Where("id = ?", quotacode.ID).First(&quotacode); tx.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "user not found"})
+		return
+	}
+
+	if err := entity.DB().Save(&quotacode).Error; err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
