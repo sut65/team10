ef208d3a9819eda5982945fa7d1a81cc0f98ad75
Taweechai Poedee <taweechaipoedee8@gmail.com>
2023-02-07 23:42:49 +0700

    add validation datetimenotnull -close #56

--- backend/test/bill_test.go
+++ backend/test/bill_test.go
@@ -52,3 +52,25 @@ func TestBillDateTimeNotPass(t *testing.T) {
 	// err.Error ต้องมี error message แสดงออกมา
 	g.Expect(err.Error()).To(Equal("เวลาห้ามเป็นอดีต"))
 }
+
+// วันเวลาห้ามเป็นอดีตเกิน5นาที
+func TestBillDateTimeNotNull(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	p := entity.Bill{
+		Bill_Price: 200,
+		Time_Stamp: time.Time{}, // ผิด 0001-01-01 00:00:00 +0000 UTC
+	}
+
+	// ตรวจสอบด้วย govalidator
+	ok, err := govalidator.ValidateStruct(p)
+
+	// ok ต้องไม่เป็นค่า true แปลว่าต้องจับ error ได้
+	g.Expect(ok).ToNot(BeTrue())
+
+	// err ต้องไม่เป็นค่า nil แปลว่าต้องจับ error ได้
+	g.Expect(err).ToNot(BeNil())
+
+	// err.Error ต้องมี error message แสดงออกมา
+	g.Expect(err.Error()).To(Equal("กรุณาใส่เวลาให้ถูกต้อง"))
+}
