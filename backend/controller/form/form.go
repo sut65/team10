09e32c372074e4f6b2a05586d10c0a214be94fb3
luxuryheart <pnkhotmail@gmail.com>
2023-02-09 02:20:26 +0700

    UpdateForm

--- backend/controller/form/form.go
+++ backend/controller/form/form.go
@@ -8,73 +8,6 @@ import (
 	"gorm.io/gorm"
 )
 
-// POST /users
-// func CreateForm(c *gin.Context) {
-
-// 	var formtype entity.FormType
-//        var satisfaction entity.Satisfaction
-//        var form entity.Form
-
-// 	if err := c.ShouldBindJSON(&form); err != nil {
-// 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-// 		return
-// 	}
-
-// 	println(form.ID)
-// 	if tx := entity.DB().Where("id = ?", form.FormTypeID).First(&formtype); tx.RowsAffected == 0 {
-// 		c.JSON(http.StatusBadRequest, gin.H{"error": "OFFicer not found"})
-// 		return
-// 	}
-// 	println(form.ID)
-
-// 	// 10. ค้นหา department ด้วย id
-// 	if tx := entity.DB().Where("id = ?", form.SatisfactionID).First(&satisfaction); tx.RowsAffected == 0 {
-// 		c.JSON(http.StatusBadRequest, gin.H{"error": "Department not found"})
-// 		return
-// 	}
-
-// 	// 13. สร้าง Form
-// 	fm := entity.Form{
-// 		Comment: form.Comment,
-//               Satisfaction: satisfaction,
-//               FormType: formtype,
-// 	}
-
-// 	if err := entity.DB().Create(&fm).Error; err != nil {
-// 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-// 		return
-// 	}
-// 	c.JSON(http.StatusOK, gin.H{"data": fm})
-// }
-
-// // GET /user/:id
-
-// func GetForm(c *gin.Context) {
-
-// 	var form entity.Form
-
-// 	id := c.Param("id")
-
-// 	if err := entity.DB().Raw("SELECT * FROM froms WHERE id = ?", id).Scan(&form).Error; err != nil {
-// 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-// 		return
-// 	}
-// 	c.JSON(http.StatusOK, gin.H{"data": form})
-// }
-
-// GET /users
-
-// func ListForms(c *gin.Context) {
-
-// 	var form []entity.Form
-
-// 	if err := entity.DB().Preload("FormType").Preload("").Preload("Satisfaction").Raw("SELECT * FROM forms").Find(&form).Error; err != nil {
-// 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-// 		return
-// 	}
-// 	c.JSON(http.StatusOK, gin.H{"data": form})
-// }
-
 type extendedForm struct {
 	entity.Form
 	Comment string
@@ -184,7 +117,7 @@ func DeleteForm(c *gin.Context) {
 // PATCH /users
 
 func UpdateForm(c *gin.Context) {
-
+	var customer entity.Customer
 	var formtype entity.FormType
 	var satisfaction entity.Satisfaction
 	var form entity.Form
@@ -220,6 +153,7 @@ func UpdateForm(c *gin.Context) {
 		Satisfaction: satisfaction,
 		FormType:     formtype,
 		Comment: updateComment,
+		Customer: customer,
 	}
 
 	if err := entity.DB().Save(&up_fmc).Error; err != nil {
